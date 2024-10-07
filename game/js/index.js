ff68bd451e10b9417d5519b67d060b035d2bb879
Vlad <Vlad>
2024-10-07 20:19:42 +0200

    feat: added fun getting columns of the array

--- game/js/index.js
+++ game/js/index.js
@@ -1,9 +1,60 @@
+//* Import
+
 import { Grid } from "./grid";
 import { Tile } from "./tile";
 
 
 const gameBoard = document.querySelector('.grid');
-
 const grid = new Grid(gameBoard)
 grid.getRandomEmptyCell().linkTile(new Tile(gameBoard))
 grid.getRandomEmptyCell().linkTile(new Tile(gameBoard))
+setupInputOnce()
+
+
+
+
+//* Functions
+
+function setupInputOnce() {
+    window.addEventListener('keydown', handleInput, {once: true})
+}
+
+function handleInput(event) {
+    switch (event.key) {
+        case 'ArrowUp':
+            moveUp()
+        break;
+        case 'ArrowDown':
+        break;
+        case 'ArrowLeft':
+        break;
+        case 'ArrowRight':
+        break;
+        
+
+        default:
+        setupInputOnce();
+        return
+    }
+    setupInputOnce()
+}
+
+
+function moveUp() {
+    slideTiles(grid.cellsGroupedByColumn)
+}
+
+function slideTiles(groupedCells) {
+groupedCells.forEach(group => slidetilesIngroup(group));
+
+}
+
+function slidetilesIngroup(group) {
+for(let i = 1; i < group.length; i++) {
+    if(group[i].isEmpty()) {
+        continue;
+    }
+
+    const cellWithTile = group[i]
+}
+}
